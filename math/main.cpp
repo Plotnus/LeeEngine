d14c9e4096d1652c4fa2c09b0059f757668eec15
plot <plotnus@gmail.com>
2016-03-04 03:56:12 -0800

    Removed .swp, and updated main test

--- math/main.cpp
+++ math/main.cpp
@@ -1,7 +1,7 @@
-//#include "mat3f.h"
-//#include "vec3f.h"
-#include "vec4f.h"
-#include "mat4f.h"
+#include "mat3f.h"
+#include "vec3f.h"
+//#include "vec4f.h"
+//#include "mat4f.h"
 #include <iostream>
 #include <string>
 
@@ -10,24 +10,24 @@ int main()
     std::cout<<"LeeEngine says, \"Hello World!\""<<std::endl;
     std::cout<<std::endl;
     
-//    vec3f vector = vec3f();
-//    std::cout << "Expeted Result: 0 vector" << std::endl;
-//    std::cout << "Actual Result:" << std::endl;
-//    std::cout << vector.getString();
-    
-//    mat3f mat3= mat3f();
-//    std::cout << "Expeted Result: 0 mat3f" << std::endl;
-//    std::cout << "Actual Result:" << std::endl;
-//    std::cout << mat3.getString();
-    
-    vec4f vec4 = vec4f();
+    vec3f vector = vec3f();
     std::cout << "Expeted Result: 0 vector" << std::endl;
     std::cout << "Actual Result:" << std::endl;
-    std::cout << vec4.getString() << std::endl;
+    std::cout << vector.getString();
     
-    mat4f mat4= mat4f();
-    std::cout << "Expeted Result: 0 mat4f" << std::endl;
+    mat3f mat3= mat3f();
+    std::cout << "Expeted Result: 0 mat3f" << std::endl;
     std::cout << "Actual Result:" << std::endl;
-    std::cout << mat4.getString();
+    std::cout << mat3.getString();
+    
+//    vec4f vec4 = vec4f();
+//    std::cout << "Expeted Result: 0 vector" << std::endl;
+//    std::cout << "Actual Result:" << std::endl;
+//    std::cout << vec4.getString() << std::endl;
+    
+//    mat4f mat4= mat4f();
+//    std::cout << "Expeted Result: 0 mat4f" << std::endl;
+//    std::cout << "Actual Result:" << std::endl;
+//    std::cout << mat4.getString();
     
 }
